d7514132313dc87dcf113c6c7f7b132c867aa4bb
Scott Harvey <scottyharvey33@gmil.com>
2016-06-27 10:46:06 -0600

    Got a couple endpoints working. Make sure to remember mongoose.connect

--- server/server.js
+++ server/server.js
@@ -4,11 +4,34 @@ var cors = require("cors");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var app = express();
-
-
-
+var User = require("./models/userSchema.js")
+var Wod = require("./models/wod.js")
+mongoose.connect("mongodb://localhost/person-project")
+app.use(bodyParser.json())
 app.use(express.static("../public"))
 
+app.get("/api/test", function(req, res, next){
+  User.find(function(err, ans){
+    if(err){
+      res.status(500).json(err)
+    }
+    else{
+    res.status(200).json(ans)
+    }
+  })
+})
+
+app.post("/api/post", function(req, res, next){
+
+  User.create(req.body, function(err, ans){
+    if(err){
+      res.status(500).json(err)
+    }
+    else{
+    res.status(200).json(ans)
+    }
+  })
+})
 
 
 
